00e2f8945903301d210dfb5cc281a0c569bd7da4
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-18 10:51:29 -0800

    added inital html code and the UI log for scripts

--- js/scripts.js
+++ js/scripts.js
@@ -14,24 +14,6 @@ function wordCounter(text) {
   return wordCount;
 }
 
-// function numberOfOccurrencesInText(word, text) {
-//   const textArray = text.split(" ");
-//   let wordCount = 0;
-//   textArray.forEach(function(element) {
-//     if (word.toLowerCase() === element.toLowerCase()) {
-//       wordCount++;
-//     }
-//   });
-//   return wordCount;
-// }
-
-// function includesRarestLetter(word) {
-//   if (word.toLowerCase().includes("q")) {
-//     return true;
-//   }
-//   return false;
-// }
-
 function numberOfOccurrencesInText(word, text) {
   const textArray = text.split(" ");
   let wordCount = 0;
@@ -42,3 +24,19 @@ function numberOfOccurrencesInText(word, text) {
   });
   return wordCount;
 }
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
